0a4bc333ce691b06921ef4803cc9dd46ef9342ac
hjs6082 <70927848+hjs6082@users.noreply.github.com>
2021-11-07 22:40:15 +0900

    터치시 돈획득

--- Assets/Scripts/Etc/TouchMoney.cs
+++ Assets/Scripts/Etc/TouchMoney.cs
@@ -6,10 +6,22 @@ public class TouchMoney : MonoBehaviour
 {
     public Animator ani;
     bool open = true;
+    GameManager gma;
+
+    public void Start()
+    {
+        gma = GameManager.Instance;
+    }
+    public void Update()
+    {
+    }
 
     public void Click()
     {
         ani.SetInteger("open", open ? 1 : 2);
         open = !open;
+
+        gma.savedData.userInfo.silver += gma.savedData.userInfo.currentSilver;
+        gma.savedData.userInfo.currentSilver = 0;
     }
 }
